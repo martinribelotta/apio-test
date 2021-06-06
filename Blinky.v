4b349aded32b859df8d4512fc6ec06bc329162fb
Martin Ribelotta <martinribelotta@gmail.com>
2021-06-05 23:20:20 -0300

    Add simulation
    Signed-off-by: Martin Ribelotta <martinribelotta@gmail.com>

--- Blinky.v
+++ Blinky.v
@@ -4,13 +4,16 @@
 
 module Test (
   input CLK,    // 12MHz clock
-  output LED0   // D6
+  output LED0,   // D6
+  output LED1,   // D7
+  output LED2,   // D8
+  output LED3    // D9
 );
   reg [23:0] counter = 0;
 
   always @(posedge CLK) 
     counter <= counter + 1;
 
-  assign LED0 = counter[23];
+  assign {LED0, LED1, LED2, LED3} = counter[23:20];
 
 endmodule
